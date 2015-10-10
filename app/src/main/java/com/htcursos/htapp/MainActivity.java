86c78a301de03c7874e5433b39a97a339470e61c
Vinicius Lima <viniciuslm@hightechcursos.com.br>
2015-10-10 07:52:58 -0400

    commit inicial

---
+++ app/src/main/java/com/htcursos/htapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.htcursos.htapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by viniciuslm on 03/10/15.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
